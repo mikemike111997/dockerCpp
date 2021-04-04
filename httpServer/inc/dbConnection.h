76c87e4c7657e2be2181de1ace6d02e50b9ae5bf
Mykhailo Lohvynenko <mikemike111997@gmail.com>
2021-04-04 15:00:19 +0300

    Add postgres support.

---
+++ httpServer/inc/dbConnection.h
@@ -0,0 +1,53 @@
+#pragma once
+
+/**
+ * @file dbConnection.h
+ * @author Mykhailo Lohvynenko (mikemike111997@gmail.com)
+ * @brief Wrapper class for SQL interaction
+ * @version 0.1
+ * @date 2021-04-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <string>
+#include <string_view>
+#include <libpq-fe.h>
+#include <memory>
+
+namespace NSDBConnection
+{
+    class PGConnection final
+    {
+    public:
+        PGConnection(std::string host, std::string port, std::string dbName,
+                    std::string dbUser, std::string dbPass);
+
+        PGConnection(const PGConnection&) = delete;
+        
+        PGConnection& operator=(const PGConnection&) = delete;
+        
+        std::shared_ptr<PGconn> connection() const { return connection_; }
+
+        PGresult* executeSQL(std::string_view sqlStr);
+
+    private:
+        void connect(); 
+
+    private:
+        std::string host_;
+
+        std::string port_;
+        
+        std::string dbName_;
+        
+        std::string dbUser_;
+        
+        std::string dbPass_;
+
+        std::shared_ptr<PGconn> connection_;
+    };
+
+    std::string htmlTableFromRes(PGresult* res);
+}
